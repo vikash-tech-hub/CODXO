47e1be919b47524035af1f522d66cfe9a906aa1e
vikash-tech-hub <vtech076@gmail.com>
2024-09-23 22:09:02 +0530

    Update Currency_Convertor.py

--- Currency_Convertor.py
+++ Currency_Convertor.py
@@ -1,1 +1,10 @@
-from currrency_converter import CurrencyConverter
+from currency_converter import CurrencyConverter
+print("this is a currecy converter project")
+print("you can use these currency ('INR','USD','EUR'.'JPY')")
+amount=float(input(print("enter the amount you want to convert: ")))
+cu_currency=input("enter the currency which you currently have :")
+want_currency=input("enter the currency you want to have : ")
+c=CurrencyConverter()
+coverted_amount=c.convert(amount,cu_currency,want_currency)
+print(f"The {amount} {cu_currency}in {want_currency} is: { coverted_amount}")
+
