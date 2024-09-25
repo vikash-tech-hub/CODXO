09ef774f141d15678d56ad4b18c8f1425e813c3e
vikash-tech-hub <vtech076@gmail.com>
2024-09-25 20:14:34 +0530

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,1 +1,8 @@
+import random 
+char="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*(){}[]"
+length=int(input("enter the password length"))
+password=""
+for i in range(length):
+    password+=random.choice(char)
+print(password)
 
